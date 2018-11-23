d929927ac97492827d9f0448b5c783b48e1130c8
Brandon Joo <bjoo@lcs.on.ca>
2018-11-23 10:58:00 -0500

    line art recent work

--- Playground/Playground.playground/Pages/Line Art.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Line Art.xcplaygroundpage/Contents.swift
@@ -20,29 +20,47 @@ canvas.drawing { t in
             
         }
     }
-    for _ in 1...10 {
+    for _ in 1...5 {
         t.right(18)
     
         
-        for _ in 1...10 {
+        for _ in 1...5 {
             t.right(18)
             t.curve(withSide: 5, withSize: 30, drawSides: 5)
         }
     }
     
-    for _ in 1...10 {
+    for _ in 1...18 {
+        t.goto(0, 0)
+        t.curve(withSide: 1, withSize: 155, drawSides: 1)
+        t.right(20)
+    }
+
+    for _ in 1...36{
+        t.goto(0, 0)
+        t.curve(withSide: 3, withSize: 30, drawSides: 3)
+        t.right(10)
+    }
+    
+    for _ in 1...36{
+        t.goto(0, 0)
+        t.curve(withSide: 5, withSize: 70, drawSides: 5)
+        t.right(10)
+    }
+    for _ in 1...3 {
+        t.goto(0, 0)
         t.right(20)
         
-        for _ in 1...10 {
+        for _ in 1...6 {
             t.right(20)
             t.curve(withSide: 10, withSize: 50, drawSides: 10)
         }
     }
 
-    for _ in 1...36 {
-        t.right(10)
-        t.forward(10)
-    }
+    for _ in 1...5{
+        t.goto(0, 0)
+        
 }
+
 
 
